642aefbbbece417470fbb8e007bb9bd599b0bc8a
Andrew Davis <atd285@gmail.com>
2021-04-04 13:26:54 -0400

    add a proper logo

--- 01-marketing-site/src/components/header.tsx
+++ 01-marketing-site/src/components/header.tsx
@@ -113,7 +113,7 @@ const Header = ({ siteTitle }: HeaderProps) => (
   >
 
     <nav style={navStyles}>
-      <div style={firstChildDiv}><a style={anchorStyles} href="">Logo
+      <div style={firstChildDiv}><a style={anchorStyles} href=""><svg width="32" height="32" class="_1Jlgk" version="1.1" viewBox="0 0 32 32" aria-labelledby="unsplash-home" aria-hidden="false"><title id="unsplash-home">Unsplash Home</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg>
         <div
           style={{ display: 'flex', flexDirection: 'column', marginLeft: '12px', whiteSpace: 'nowrap' }}
         ><span><strong>{siteTitle}</strong></span>
