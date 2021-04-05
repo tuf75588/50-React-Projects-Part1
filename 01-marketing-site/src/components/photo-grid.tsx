ac8e9bf11cdce101134e87f9283455072d8bfebb
Andrew Davis <atd285@gmail.com>
2021-04-05 13:59:14 -0400

    masonry-grid layout for demo images

--- 01-marketing-site/src/components/photo-grid.tsx
+++ 01-marketing-site/src/components/photo-grid.tsx
@@ -9,9 +9,21 @@ function PhotoGrid() {
       <div>
         <div>
           <div className="items">
-            <div><img src={photoOne} alt="demo one" /></div>
-            <div><img src={photoTwo} alt="demo two" /></div>
-            <div><img src={photoThree} alt="demo three" /></div>
+            <div className="item">
+              <img src={photoOne} alt="demo one" />
+              <img src={photoTwo} alt="demo two" />
+              <img src={photoThree} alt="demo three" />
+            </div>
+            <div className="item">
+              <img src={photoTwo} alt="demo two" />
+              <img src={photoThree} alt="demo three" />
+              <img src={photoOne} alt="demo one" />
+            </div>
+            <div className="item">
+              <img src={photoOne} alt="demo one" />
+              <img src={photoThree} alt="demo three" />
+              <img src={photoTwo} alt="demo two" />
+            </div>
           </div>
         </div>
       </div>
