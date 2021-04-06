1c144ee79d9569a2d5efc46720396944dc3bb68f
Andrew Davis <atd285@gmail.com>
2021-04-06 15:26:09 -0400

    finish landing page

--- 01-marketing-site/src/components/hero.tsx
+++ 01-marketing-site/src/components/hero.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image'
 function Hero() {
   return (
-    <div className="hero-container" style={{ width: '100vw', padding: 0, position: 'relative' }}>
-      <div style={{ position: 'absolute', fontWeight: 700, zIndex: 1, top: '20%', left: '30%', fontSize: '2.5rem', color: '#fff' }}>
-        <span>Nusplash</span>
-      </div>
-      <div style={{ position: 'absolute', zIndex: 2, display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%', alignItems: 'center', width: '19%', paddingRight: '12px', top: '-13%', left: '30%', color: '#fff' }}>
-        <h1 style={{ fontSize: '18px', fontWeight: 400, }}>The internet’s source of freely-usable images.</h1>
+    <div className="hero-container" style={{ padding: 0, position: 'relative' }}>
+
+      <div className="flex-containe" style={{ position: 'absolute', display: 'flex', flexDirection: 'column', zIndex: '1', height: '70%', justifyContent: 'center', left: '30%', color: '#fff', width: '60%' }}>
+        <h1 style={{ fontWeight: 700 }}>Nusplash</h1>
+        <span className="subtext">The internet’s source of freely-usable images.</span>
+        <p className="subtext">Powered by creators everywhere.</p>
+        <input type="text" name="search" placeholder="Search images" style={{ padding: '20px', width: '60%', borderRadius: '5px', border: 0, marginLeft: '-3px' }} />
+        <p>Trending: <span style={{
+          color: 'hsla(0,0%,100%,.8)'
+        }}>flower, wallpapers, backgrounds, happy, love</span></p>
       </div>
       <StaticImage
         src="../images/hero.jpg"
@@ -16,8 +20,10 @@ function Hero() {
         alt="Nusplash hero"
         style={{ marginBottom: `1.45rem` }}
       />
-    </div>
+    </div >
   )
 }
 
 export default Hero;
+
+
