fbce619077a59c5c05c754fb1e7378925781076c
Andrew Davis <atd285@gmail.com>
2021-04-08 18:24:14 -0400

    fix some alignment issues on navbar

--- 02-conference-schedule/src/shared/header.tsx
+++ 02-conference-schedule/src/shared/header.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-
 function Header() {
   return (
     <header className="header_home">
       <div className="header-nav">
         <section>
-          <a href="/" className="home-link">Coolest React Conf</a>
           <ul>
             <li><a href="/speakers">Speakers</a></li>
             <li><a href="/schedule">Schedule</a></li>
@@ -15,13 +13,18 @@ function Header() {
             <li><a href="/about">About</a></li>
           </ul>
         </section>
-        <div className="header-nav-social">
-          <a href="http://" target="_blank" rel="noopener noreferrer">Twitter</a>
-          <a href="http://" target="_blank" rel="noopener noreferrer">Github</a>
-          <a href="http://" target="_blank" rel="noopener noreferrer">Instagram</a>
-          <a href="http://" target="_blank" rel="noopener noreferrer">YouTube</a>
-          <a href="http://" target="_blank" rel="noopener noreferrer">Tickets</a>
-        </div>
+        <section className="header-nav-social">
+          <a href="http://" className="Icon" target="_blank" rel="noopener noreferrer">
+
+          </a>
+          <a href="http://" className="Icon" target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-github"></i></a>
+          <a href="http://" className="Icon" target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-instagram"></i></a>
+          <a href="http://" className="Icon" target="_blank" rel="noopener noreferrer">
+            <i className="fa fa-youtube"></i></a>
+          <a href="http://" className="Button" target="_blank" rel="noopener noreferrer">Tickets</a>
+        </section>
       </div>
     </header>
   )
