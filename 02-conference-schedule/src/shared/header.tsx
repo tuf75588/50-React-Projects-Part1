1e6e221461e785db37d32f5e148030db4079bd93
Andrew Davis <atd285@gmail.com>
2021-04-09 14:44:54 -0400

    header for site 2 done ✅

--- 02-conference-schedule/src/shared/header.tsx
+++ 02-conference-schedule/src/shared/header.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import mockLogo from '../images/mock_logo.png'
 function Header() {
   return (
     <header className="header_home">
+      <div className="blm_banner">Black Lives Matter</div>
       <div className="header-nav">
         <section>
           <ul>
@@ -25,6 +27,16 @@ function Header() {
             <i className="fa fa-youtube"></i></a>
           <a href="http://" className="Button" target="_blank" rel="noopener noreferrer">Tickets</a>
         </section>
+      </div>
+      <div className="home_header">
+        <div className="home_header_wrapper">
+          <img src={mockLogo} alt="Mock conference logo" className="home_header_logo" />
+          <div className="home_header_content">
+            <h1>Awesome React Conf</h1>
+            <h2>{new Date().toDateString()}</h2>
+            <p>Awesome React Conf is a community conference about React and topics interesting to React developers. We focus on a friendly, welcoming atmosphere, engaging talks from new and established speakers, and plenty of hallway-track time to chat with interesting people.</p>
+          </div>
+        </div>
       </div>
     </header>
   )
