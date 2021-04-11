16817217027e5e37594b77491a6020b19ca64ce4
Andrew Davis <atd285@gmail.com>
2021-04-11 15:54:21 -0400

    render some new products

--- 03-commerce-cms/src/pages/index.js
+++ 03-commerce-cms/src/pages/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import Layout from "../layouts/index"
-import Img from 'gatsby-image'
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+import Layout from '../layouts/index';
+import Img from 'gatsby-image';
 
 export default () => (
   <StaticQuery
@@ -15,7 +15,11 @@ export default () => (
               price
               image {
                 url
-                sizes(maxWidth: 300, imgixParams: { fm: "jpg" }) {
+                sizes(
+                  maxWidth: 300
+                  maxHeight: 300
+                  imgixParams: { fm: "jpg" }
+                ) {
                   ...GatsbyDatoCmsSizes
                 }
               }
@@ -29,37 +33,34 @@ export default () => (
         }
       }
     `}
-render={data => (
-  <Layout site={data.site}>
-    <div className="Catalogue">
-      {
-        data.products.edges.map(({ node: product }) => (
-          <div className="Catalogue__item" key={product.id}>
-            <div
-              className="Product snipcart-add-item"
-              data-item-id={product.id}
-              data-item-price={product.price}
-              data-item-image={product.image.url}
-              data-item-name={product.name}
-              data-item-url={`/`}
-            >
-              <div className="Product__image">
-                <Img sizes={product.image.sizes} />
-              </div> <div className="Product__details">
-                <div className="Product__name">
-                  {product.name}
-                  <div className="Product__price">
-                    {product.price}€
+    render={(data) => (
+      <Layout site={data.site}>
+        <div className="Catalogue">
+          {data.products.edges.map(({ node: product }) => (
+            <div className="Catalogue__item" key={product.id}>
+              <div
+                className="Product snipcart-add-item"
+                data-item-id={product.id}
+                data-item-price={product.price}
+                data-item-image={product.image.url}
+                data-item-name={product.name}
+                data-item-url={`/`}
+              >
+                <div className="Product__image">
+                  <Img sizes={product.image.sizes} />
+                </div>{' '}
+                <div className="Product__details">
+                  <div className="Product__name">
+                    {product.name}
+                    <div className="Product__price">{product.price}€</div>
                   </div>
+                  <span className="Product__buy">Buy now</span>
                 </div>
-                <span className="Product__buy">Buy now</span>
               </div>
             </div>
-          </div>
-        ))
-      }
-    </div>
-  </Layout>
-     )}
-   />
-)
+          ))}
+        </div>
+      </Layout>
+    )}
+  />
+);
