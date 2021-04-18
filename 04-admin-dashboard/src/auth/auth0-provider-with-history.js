ae2c8fe6bb15025424b127f39ebaca519e9f38a9
Andrew Davis <atd285@gmail.com>
2021-04-18 15:03:31 -0400

    add history provider

---
+++ 04-admin-dashboard/src/auth/auth0-provider-with-history.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Auto0Provider } from '@auth0/auth0-react';
+
+function Auth0ProviderWithHistory({ children }) {
+  const history = useHistory();
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+  const onRedirectCallback = (appState) => {
+    history.push(appState?.returnTo || window.location.pathname);
+  };
+  return (
+    <Auto0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auto0Provider>
+  );
+}
+
+export default Auth0ProviderWithHistory;
--- 04-admin-dashboard/src/index.js
+++ 04-admin-dashboard/src/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Auth0Provider } from '@auth0/auth0-react';
+
 import './index.css';
+import Auth0ProviderWithHistory from './auth/auth0-provider-with-history';
 
 ReactDOM.render(
   <Router>
-    <Auth0Provider>
+    <Auth0ProviderWithHistory>
       <App />
-    </Auth0Provider>
+    </Auth0ProviderWithHistory>
   </Router>,
   document.getElementById('root')
 );
