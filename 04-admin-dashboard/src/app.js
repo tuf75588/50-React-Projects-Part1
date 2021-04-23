86fca684a749ac23662aa9cc5c52196d3419da68
Andrew Davis <atd285@gmail.com>
2021-04-23 14:39:50 -0400

    add a loading spinner while SDK is loading

--- 04-admin-dashboard/src/app.js
+++ 04-admin-dashboard/src/app.js
@@ -1,12 +1,16 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import { NavBar, Footer } from "./components";
-import { Home, Profile, ExternalApi } from "./views";
-
-import "./app.css";
+import { NavBar, Footer, Loading } from './components';
+import { Home, Profile, ExternalApi } from './views';
+import { useAuth0 } from '@auth0/auth0-react';
+import './app.css';
 
 const App = () => {
+  const { isLoading } = useAuth0();
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <div id="app" className="d-flex flex-column h-100">
       <NavBar />
