9e3091a50e272dce64b4006596ae2c1ae0b157fe
Andrew Davis <atd285@gmail.com>
2021-04-30 13:44:09 -0400

    better navbar for admin view

---
+++ 04-admin-dashboard/client/src/components/admin-nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AdminNavbar() {
+  return (
+    <header className="admin-nav">
+      <div className="admin-nav-first-child">
+        <h1 className="admin_header">Admin dash</h1>
+      </div>
+    </header>
+  );
+}
+
+export default AdminNavbar;
--- 04-admin-dashboard/client/src/views/home.js
+++ 04-admin-dashboard/client/src/views/home.js
@@ -1,13 +1,18 @@
 import React, { Fragment } from 'react';
-
+import { useAuth0 } from '@auth0/auth0-react';
 import { Hero, HomeContent, NavBar } from '../components';
 import Header from '../components/header';
+import AdminNavbar from '../components/admin-nav';
 
-const Home = () => (
-  <Fragment>
-    <NavBar />
-    <Hero />
-  </Fragment>
-);
+const Home = () => {
+  const { isAuthenticated } = useAuth0();
+  console.log(isAuthenticated);
+  return (
+    <Fragment>
+      {!isAuthenticated ? <NavBar /> : <AdminNavbar />}
+      <Hero />
+    </Fragment>
+  );
+};
 
 export default Home;
