70f399f3b7bb9d38ce993379e1cb0c8ce90fa8b9
Andrew Davis <atd285@gmail.com>
2021-05-02 17:32:58 -0400

    better svgs for each nav item

--- 04-admin-dashboard/client/src/components/side-bar.js
+++ 04-admin-dashboard/client/src/components/side-bar.js
@@ -1,5 +1,15 @@
 import React from 'react';
-
+import Security, {
+  Actions,
+  Activity,
+  Applications,
+  Authentication,
+  AuthPipeline,
+  Branding,
+  Monitoring,
+  Organizations,
+  UserManagement,
+} from '../icons';
 function Sidebar() {
   return (
     <div className="dash-login">
@@ -32,266 +42,17 @@ function Sidebar() {
                 </span>
               </div>
             </a>
-            <a
-              class="MuiListItem-root jss76 MuiListItem-gutters Mui-selected jss79"
-              role="listitem"
-              href="/dashboard/us/admin-dashboards/"
-            >
-              <div class="MuiListItemIcon-root jss80">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
-                </svg>
-              </div>
-              <div class="MuiListItemText-root">
-                <span class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
-                  Getting Started
-                </span>
-              </div>
-            </a>
-            <a
-              class="MuiListItem-root jss76 MuiListItem-gutters Mui-selected jss79"
-              role="listitem"
-              href="/dashboard/us/admin-dashboards/"
-            >
-              <div class="MuiListItemIcon-root jss80">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
-                </svg>
-              </div>
-              <div class="MuiListItemText-root">
-                <span class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
-                  Getting Started
-                </span>
-              </div>
-            </a>
-            <a
-              class="MuiListItem-root jss76 MuiListItem-gutters Mui-selected jss79"
-              role="listitem"
-              href="/dashboard/us/admin-dashboards/"
-            >
-              <div class="MuiListItemIcon-root jss80">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
-                </svg>
-              </div>
-              <div class="MuiListItemText-root">
-                <span class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
-                  Getting Started
-                </span>
-              </div>
-            </a>
-            <a
-              class="MuiListItem-root jss76 MuiListItem-gutters Mui-selected jss79"
-              role="listitem"
-              href="/dashboard/us/admin-dashboards/"
-            >
-              <div class="MuiListItemIcon-root jss80">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
-                </svg>
-              </div>
-              <div class="MuiListItemText-root">
-                <span class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
-                  Getting Started
-                </span>
-              </div>
-            </a>
-            <a
-              class="MuiListItem-root jss76 MuiListItem-gutters Mui-selected jss79"
-              role="listitem"
-              href="/dashboard/us/admin-dashboards/"
-            >
-              <div class="MuiListItemIcon-root jss80">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
-                </svg>
-              </div>
-              <div class="MuiListItemText-root">
-                <span class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
-                  Getting Started
-                </span>
-              </div>
-            </a>
-            <a
-              class="MuiListItem-root jss76 MuiListItem-gutters Mui-selected jss79"
-              role="listitem"
-              href="/dashboard/us/admin-dashboards/"
-            >
-              <div class="MuiListItemIcon-root jss80">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
-                </svg>
-              </div>
-              <div class="MuiListItemText-root">
-                <span class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
-                  Getting Started
-                </span>
-              </div>
-            </a>
-            <a
-              class="MuiListItem-root jss76 MuiListItem-gutters Mui-selected jss79"
-              role="listitem"
-              href="/dashboard/us/admin-dashboards/"
-            >
-              <div class="MuiListItemIcon-root jss80">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
-                </svg>
-              </div>
-              <div class="MuiListItemText-root">
-                <span class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
-                  Getting Started
-                </span>
-              </div>
-            </a>
-            <a
-              class="MuiListItem-root jss76 MuiListItem-gutters Mui-selected jss79"
-              role="listitem"
-              href="/dashboard/us/admin-dashboards/"
-            >
-              <div class="MuiListItemIcon-root jss80">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
-                </svg>
-              </div>
-              <div class="MuiListItemText-root">
-                <span class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
-                  Getting Started
-                </span>
-              </div>
-            </a>
-            <a
-              class="MuiListItem-root jss76 MuiListItem-gutters Mui-selected jss79"
-              role="listitem"
-              href="/dashboard/us/admin-dashboards/"
-            >
-              <div class="MuiListItemIcon-root jss80">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
-                </svg>
-              </div>
-              <div class="MuiListItemText-root">
-                <span class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
-                  Getting Started
-                </span>
-              </div>
-            </a>
-            <a
-              class="MuiListItem-root jss76 MuiListItem-gutters Mui-selected jss79"
-              role="listitem"
-              href="/dashboard/us/admin-dashboards/"
-            >
-              <div class="MuiListItemIcon-root jss80">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="18"
-                  height="18"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
-                </svg>
-              </div>
-              <div class="MuiListItemText-root">
-                <span class="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
-                  Getting Started
-                </span>
-              </div>
-            </a>
+            <Activity />
+            <Applications />
+
+            <Authentication />
+            <Organizations />
+            <UserManagement />
+            <Branding />
+            <Security />
+            <Actions />
+            <AuthPipeline />
+            <Monitoring />
           </div>
           <div role="list">
             <a href="blank">Get Support</a>
---
+++ 04-admin-dashboard/client/src/icons/index.js
@@ -0,0 +1,342 @@
+import React from 'react';
+
+export function Activity() {
+  return (
+    <a
+      className="MuiListItem-root jss76 MuiListItem-gutters"
+      role="listitem"
+      href="/dashboard/us/admin-dashboards/activity"
+    >
+      <div className="MuiListItemIcon-root jss80">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline>
+          <polyline points="17 6 23 6 23 12"></polyline>
+        </svg>
+      </div>
+      <div className="MuiListItemText-root">
+        <span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
+          Activity
+        </span>
+      </div>
+    </a>
+  );
+}
+
+export function Applications() {
+  return (
+    <a
+      className="MuiListItem-root jss76 MuiListItem-gutters"
+      role="listitem"
+      href="__blank"
+    >
+      <div className="MuiListItemIcon-root jss80">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
+          <polyline points="2 17 12 22 22 17"></polyline>
+          <polyline points="2 12 12 17 22 12"></polyline>
+        </svg>
+      </div>
+      <div className="MuiListItemText-root">
+        <span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
+          Applications
+        </span>
+      </div>
+    </a>
+  );
+}
+
+export function Authentication() {
+  return (
+    <a
+      className="MuiListItem-root jss76 MuiListItem-gutters"
+      role="listitem"
+      href="__blank"
+    >
+      <div className="MuiListItemIcon-root jss80">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+          <path d="M7 11V7a5 5 0 0 1 9.9-1"></path>
+        </svg>
+      </div>
+      <div className="MuiListItemText-root">
+        <span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
+          Authentication
+        </span>
+      </div>
+    </a>
+  );
+}
+export function Organizations() {
+  return (
+    <a
+      className="MuiListItem-root jss76 MuiListItem-gutters"
+      role="listitem"
+      href="/dashboard/us/admin-dashboards/organizations"
+    >
+      <div className="MuiListItemIcon-root jss80">
+        <svg
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M19 22V2H12H5V22M19 22H5M19 22H21M19 22H14M5 22H3M5 22H10M14 22V17C14 16.7348 13.8946 16.4804 13.7071 16.2929C13.5196 16.1054 13.2652 16 13 16H11C10.7348 16 10.4804 16.1054 10.2929 16.2929C10.1054 16.4804 10 16.7348 10 17V22M14 22H10M9 7H10M9 11H10M14 7H15M14 11H15"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+        </svg>
+      </div>
+      <div className="MuiListItemText-root">
+        <span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
+          Organizations
+        </span>
+      </div>
+    </a>
+  );
+}
+
+export function UserManagement() {
+  return (
+    <a
+      className="MuiListItem-root jss76 MuiListItem-gutters"
+      role="listitem"
+      href="__blank"
+    >
+      <div className="MuiListItemIcon-root jss80">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+          <circle cx="12" cy="7" r="4"></circle>
+        </svg>
+      </div>
+      <div className="MuiListItemText-root">
+        <span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
+          User Management
+        </span>
+      </div>
+    </a>
+  );
+}
+
+export function Branding() {
+  return (
+    <a
+      className="MuiListItem-root jss76 MuiListItem-gutters"
+      href="__blank"
+      role="listitem"
+    >
+      <div className="MuiListItemIcon-root jss80">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
+          <polyline points="10 17 15 12 10 7"></polyline>
+          <line x1="15" y1="12" x2="3" y2="12"></line>
+        </svg>
+      </div>
+      <div className="MuiListItemText-root">
+        <span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
+          Branding
+        </span>
+      </div>
+    </a>
+  );
+}
+
+export default function Security() {
+  return (
+    <a
+      class="MuiListItem-root jss76 MuiListItem-gutters"
+      href="__blank"
+      role="listitem"
+    >
+      <div className="MuiListItemIcon-root jss80">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+        </svg>
+      </div>
+      <div className="MuiListItemText-root">
+        <span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
+          Security
+        </span>
+      </div>
+    </a>
+  );
+}
+
+export function Actions() {
+  return (
+    <a
+      className="MuiListItem-root jss76 MuiListItem-gutters"
+      href="__blank"
+      role="listitem"
+    >
+      <div class="MuiListItemIcon-root jss80">
+        <i size="20" class="sc-eCApGN gPrzmg">
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            color="currentColor"
+            class="sc-fSvWoh ftMOYG"
+          >
+            <g
+              id="icon/flow"
+              stroke="none"
+              stroke-width="1"
+              fill="none"
+              fill-rule="evenodd"
+            >
+              <path d="M15.07,19.74a4.18,4.18,0,0,1-4.18-4.18V8.2A2.73,2.73,0,0,0,8.16,5.47h0A2.73,2.73,0,0,0,5.44,8.2v5.2H3.94V8.2A4.23,4.23,0,0,1,8.16,4h0A4.24,4.24,0,0,1,12.39,8.2v7.36a2.68,2.68,0,1,0,5.36,0V10.32h1.5v5.24A4.18,4.18,0,0,1,15.07,19.74Z"></path>
+              <path d="M7.44,21.47h-5a.74.74,0,0,1-.75-.75v-5A.75.75,0,0,1,2.44,15h5a.76.76,0,0,1,.75.75v5A.75.75,0,0,1,7.44,21.47ZM3.19,20h3.5v-3.5H3.19ZM21,8.75H16A.76.76,0,0,1,15.25,8V3A.76.76,0,0,1,16,2.25h5a.75.75,0,0,1,.75.75V8A.75.75,0,0,1,21,8.75Zm-4.25-1.5h3.5V3.75h-3.5Z"></path>
+            </g>
+          </svg>
+        </i>
+      </div>
+      <div className="MuiListItemText-root">
+        <span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
+          Actions
+        </span>
+      </div>
+    </a>
+  );
+}
+
+export function AuthPipeline() {
+  return (
+    <a
+      className="MuiListItem-root jss76 MuiListItem-gutters"
+      href="__blank"
+      role="listitem"
+    >
+      <div className="MuiListItemIcon-root jss80">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <polyline points="16 3 21 3 21 8"></polyline>
+          <line x1="4" y1="20" x2="21" y2="3"></line>
+          <polyline points="21 16 21 21 16 21"></polyline>
+          <line x1="15" y1="15" x2="21" y2="21"></line>
+          <line x1="4" y1="4" x2="9" y2="9"></line>
+        </svg>
+      </div>
+      <div className="MuiListItemText-root">
+        <span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
+          Auth Pipeline
+        </span>
+      </div>
+    </a>
+  );
+}
+
+export function Monitoring() {
+  return (
+    <a className="MuiListItem-root jss76 MuiListItem-gutters" role="listitem">
+      <div className="MuiListItemIcon-root jss80">
+        <svg width="18" height="18" fill="none" viewBox="0 0 24 24">
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M10 3H14V21H10V3Z"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M18 8H22V21H18V8Z"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M2 13H6V21H2V13Z"
+            stroke="currentColor"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+        </svg>
+      </div>
+      <div className="MuiListItemText-root">
+        <span className="MuiTypography-root MuiListItemText-primary MuiTypography-body1 MuiTypography-displayBlock">
+          Monitoring
+        </span>
+      </div>
+    </a>
+  );
+}
