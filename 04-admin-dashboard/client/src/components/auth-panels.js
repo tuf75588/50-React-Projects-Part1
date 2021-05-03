a38e9a3b2635eb08f773ee5b88c78040a49c5143
Andrew Davis <atd285@gmail.com>
2021-05-03 19:48:17 -0400

    some new fieldsets in the admin dashboard done

--- 04-admin-dashboard/client/src/components/auth-panels.js
+++ 04-admin-dashboard/client/src/components/auth-panels.js
@@ -7,6 +7,114 @@ function AuthPanels() {
       <h4>application_name</h4>
       <ApplicationNavigation />
       <hr />
+      <div className="panel-content">
+        <div className="grid-container">
+          <form noValidate>
+            <legend className="legend">Basic Information</legend>
+            <div className="outer">
+              <div className="first-form-section">
+                <div>
+                  <label>Name</label>
+                  <div>
+                    <input
+                      type="text"
+                      className="form_control_input"
+                      placeholder="admin_dashboard"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="first-form-section">
+                <div>
+                  <label>Domain</label>
+                  <div>
+                    <input
+                      type="text"
+                      className="form_control_input"
+                      placeholder="admin-dashboards.us.auth0.com"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="first-form-section">
+                <div>
+                  <label>Client ID</label>
+                  <div>
+                    <input
+                      type="text"
+                      className="form_control_input"
+                      placeholder="l8LWMMP0DUVsWw434222f3oYMa1Ww2rt"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="first-form-section">
+                <div>
+                  <label>Client Secret</label>
+                  <div>
+                    <input
+                      type="password"
+                      className="form_control_input"
+                      placeholder="••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
+                    />
+                  </div>
+                </div>
+                <hr style={{ marginTop: '30px' }} />
+              </div>
+              <legend className="legend">Application Properties</legend>
+              <div className="first-form-section">
+                <div>
+                  <label>Name</label>
+                  <div>
+                    <input
+                      type="text"
+                      className="form_control_input"
+                      placeholder="admin_dashboard"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="first-form-section">
+                <div>
+                  <label>Domain</label>
+                  <div>
+                    <input
+                      type="text"
+                      className="form_control_input"
+                      placeholder="admin_dashboard"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="first-form-section">
+                <div>
+                  <label>Client ID</label>
+                  <div>
+                    <input
+                      type="text"
+                      className="form_control_input"
+                      placeholder="admin_dashboard"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="first-form-section">
+                <div>
+                  <label>Client Secret</label>
+                  <div>
+                    <input
+                      type="text"
+                      className="form_control_input"
+                      placeholder="admin_dashboard"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <hr style={{ marginTop: '20px' }} />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
