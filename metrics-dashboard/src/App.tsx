de92138235f0ae78bd7d4110ee5240bc6d4e1b7d
Andrew Davis <atd285@gmail.com>
2021-05-10 14:33:58 -0400

    more styling for UI components

--- metrics-dashboard/src/App.tsx
+++ metrics-dashboard/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import FilterStock from './shared/filter-area';
 import SearchBar from './shared/search-bar';
 
 function App() {
   return (
-    <div className="app_container">
-      <div className="app-container_left">
-        <SearchBar />
+    <div className="app-container_row">
+      <div className="app_container">
+        <div className="app-container_left">
+          <SearchBar />
+          <FilterStock />
+        </div>
       </div>
     </div>
   );
---
+++ metrics-dashboard/src/shared/filter-area.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/FilterStock.css';
+function FilterStock(): JSX.Element {
+  return (
+    <div className="filter-area">
+      <div className="filter-card-body">
+        <h2>Filter by Stock Code:</h2>
+      </div>
+    </div>
+  );
+}
+
+export default FilterStock;
