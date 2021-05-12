9d4a8923578598a9183c2970f204f99f64b9c774
Andrew Davis <atd285@gmail.com>
2021-05-12 13:44:57 -0400

    add checks to useEffect hooks for initial page load

--- metrics-dashboard/src/App.tsx
+++ metrics-dashboard/src/App.tsx
@@ -8,13 +8,16 @@ function App() {
   const [stockCode, setStockCode] = React.useState<string>('');
   const [chosenCode, setChosenCode] = React.useState<string>('');
   const [stockData, setStockData] = React.useState([]);
+  const [graphData, setGraphData] = React.useState([]);
   const handleStockCodeChange = ({
     target: { value },
   }: React.ChangeEvent<HTMLInputElement>) => setStockCode(value);
   const reset = () => setStockCode('');
   let startDate = Math.round(new Date().getTime() / 1000);
   let endDate = startDate - 72 * 3600;
+
   React.useEffect(() => {
+    if (!chosenCode) return;
     stock
       .get(`quote`, {
         params: {
@@ -40,9 +43,10 @@ function App() {
         },
       })
       .then((res) => {
-        console.log(res);
+        setGraphData(res.data);
       });
-  }, [setChosenCode, chosenCode]);
+  }, [setChosenCode, chosenCode, startDate, endDate]);
+
   return (
     <div className="app_container">
       <div className="row app-container_row">
@@ -56,11 +60,11 @@ function App() {
           <FilterStock />
         </div>
         <div className="app-container_right">
-          <StockChart />
+          <StockChart graphData={graphData} />
         </div>
       </div>
       <pre>{stockCode}</pre>
-      <pre>{JSON.stringify(stockData, null, 2)}</pre>
+      <pre>{JSON.stringify(graphData, null, 2)}</pre>
     </div>
   );
 }
