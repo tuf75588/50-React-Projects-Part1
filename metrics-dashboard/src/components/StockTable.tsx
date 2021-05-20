e062bfe74b569e704a7a5af9bd2d782800090e7e
Andrew Davis <atd285@gmail.com>
2021-05-19 21:41:19 -0400

    need to brainstorm better soultion for page refresh

--- metrics-dashboard/src/components/StockTable.tsx
+++ metrics-dashboard/src/components/StockTable.tsx
@@ -11,20 +11,44 @@ type StockTableProps = {
 };
 
 function StockTable({ stockSymbol }: StockTableProps) {
+  const [tableData, setTableData] = React.useState({ o: '' });
+  const [status, setStatus] = React.useState('idle');
+  React.useEffect(() => {
+    stock
+      .get('quote', {
+        params: {
+          token: process.env.REACT_APP_API_KEY,
+          symbol: stockSymbol,
+        },
+      })
+      .then((response) => {
+        setStatus('loading');
+        setTableData(response.data);
+        setStatus('success');
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setStatus('idle');
+      });
+  }, [stockSymbol]);
+  if (status === 'loading') return <h1>loading..</h1>;
+  console.log(tableData);
   return (
     <div>
       {/* flex container -- COLUMN  */}
       <div className="stock-table-header-row">
         <div>
           <ul className="stock-table-data-info">
-            <li>Opening Price</li>
+            <li>Opening price</li>
             <li>High Price</li>
             <li>Low Price</li>
             <li>Current Price</li>
             <li>Previous Closing Price</li>
           </ul>
           <ul className="stock-table-data-info stock-prices">
-            <li className="stock-price-item">123.22</li>
+            <li className="stock-price-item">${tableData.o}</li>
             <li className="stock-price-item">123.22</li>
             <li className="stock-price-item">123.22</li>
             <li className="stock-price-item">123.22</li>
