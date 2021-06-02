14a37ad52908efba759531a646de7edde3f0273f
Andrew Davis <atd285@gmail.com>
2021-06-02 13:42:54 -0400

    setup for next project

---
+++ 06-survey-creator/README.md
@@ -0,0 +1,1 @@
+# Project 6 -- Survey Creator & Manager
---
+++ 06-survey-creator/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => <div>app setup</div>;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
